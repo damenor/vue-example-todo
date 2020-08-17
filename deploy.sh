0343f80b25a42acf26f852ebf47a49dd7df4d2b5
damenor <damenor.develop@gmail.com>
2020-08-18 00:52:53 +0200

    Modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,8 @@
 # abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd dist
 
 git init
@@ -14,6 +12,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:damenor/vue-example-todo.git master:gh-pages
 
 cd -
